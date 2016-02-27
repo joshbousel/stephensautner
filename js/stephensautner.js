8005e67b8b14181b11dde5f92a98f99b4bf91547
Joshua Bousel <josh@bousel.com>
2016-02-27 16:30:00 -0500

    Added feature section and js section scrolling

--- js/stephensautner.js
+++ js/stephensautner.js
@@ -1,4 +1,17 @@
 $(function(){
+	var win = $(window);
 	
+	$('.ss-nav__item').on('click',function(e){
+		e.preventDefault();	
+		win.off('scroll');
+		var id = $(this).attr("href");
+		var selectedID = id.substr(1,id.length);
+		var scrollY = $(id).offset().top;
+		$("html, body").animate({ scrollTop: scrollY }, 500, "easeOutQuint",function(){
+			win.on("scroll",function(){
+				getScrollID();
+				});
+			});
+		});
 
 });
